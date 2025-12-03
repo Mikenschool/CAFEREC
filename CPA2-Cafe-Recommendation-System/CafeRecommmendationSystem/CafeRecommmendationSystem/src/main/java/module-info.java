6d93ac22b37e1516d6c06f8cdc09d4eab1b79760
NoidThes <dion_abeleda@dlsu.edu.ph>
2025-12-03 13:40:50 +0800

    Delete CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/CafeRecommmendationSystem directory

--- CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/CafeRecommmendationSystem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ph.edu.dlsu.lbycpei.caferecommmendationsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ph.edu.dlsu.lbycpei.caferecommmendationsystem to javafx.fxml;
-    exports ph.edu.dlsu.lbycpei.caferecommmendationsystem;
-}
--- CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/Main.java
+++
@@ -1,8 +0,0 @@
-package ph.edu.dlsu.lbycpei.caferecommmendationsystem;
-
-public class Main {
-    public static void main(String[] args) {
-        CafeSystem cafe = new CafeSystem();
-        cafe.run();
-    }
-}
