66216cfa60b4c714e0dc9544e63d2b4bad30c7ea
prvngle <zachtonducan719@gmail.com>
2025-12-04 14:26:39 +0800

    Update MenuItem.java

--- CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/MenuItem.java
+++ CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/MenuItem.java
@@ -1,9 +1,12 @@
 package ph.edu.dlsu.lbycpei.caferecommmendationsystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuItem {
     private String name;
     private double price;
-
+    private List<String> recommendedItems = new ArrayList<>();
 
     public MenuItem(String name, double price) {
         this.name = name;
@@ -13,8 +16,20 @@ public class MenuItem {
     public String getName() { return name; }
     public double getPrice() { return price; }
 
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
     @Override
     public String toString() {
         return name + " - ₱" + price;
+    }
+
+    public List<String> getRecommendedItems() {
+        return recommendedItems;
+    }
+
+    public void setRecommendedItems(List<String> list) {
+        this.recommendedItems = list != null ? list : new ArrayList<>();
     }
 }
