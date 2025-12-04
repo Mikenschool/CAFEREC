4f8b22c7d6b9aeb6573e01332abe882ef0691882
prvngle <zachtonducan719@gmail.com>
2025-12-04 14:25:53 +0800

    Update InventoryItem.java

--- CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/InventoryItem.java
+++ CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/InventoryItem.java
@@ -1,23 +1,40 @@
 package ph.edu.dlsu.lbycpei.caferecommmendationsystem;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class InventoryItem {
-    private String name;
-    private int quantity;
-
+    private StringProperty name;
+    private IntegerProperty quantity;
 
     public InventoryItem(String name, int quantity) {
-        this.name = name;
-        this.quantity = quantity;
+        this.name = new SimpleStringProperty(name);
+        this.quantity = new SimpleIntegerProperty(quantity);
     }
 
+    public String getName() {
+        return name.get();
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public StringProperty nameProperty() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name.set(name);
+    }
 
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public int getQuantity() {
+        return quantity.get();
+    }
 
+    public IntegerProperty quantityProperty() {
+        return quantity;
+    }
 
+    public void setQuantity(int quantity) {
+        this.quantity.set(quantity);
+    }
 }
